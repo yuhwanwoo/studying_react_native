f1172d90f98a835376482ebe18a1b4857c3aacc1
yuhwanwoo <6514687@naver.com>
2021-03-14 22:37:01 +0900

    props

--- lesson02/App.js
+++ lesson02/App.js
@@ -9,6 +9,7 @@ class MyComponent extends Component {
     name:"Harry",
     colors:['blue'],
     happy:false,
+    act_props2:'ACT PROPS2',
    }
   }
 
@@ -18,8 +19,15 @@ class MyComponent extends Component {
       happy:true,
     })
   }
+  updateProps(){
+    this.setState({
+      act_props2:"Change Value",
+    })
+  }
   
   render (){
+    const {act_props2}=this.state
+    let act_props='ACT PROPS'
     let leapyear=<Text>Happy New year!</Text>
     if (this.state.happy){
       leapyear=<Text>Hello world</Text>
@@ -30,9 +38,35 @@ class MyComponent extends Component {
       <Text onPress={()=>this.updateYear()}>The year is: {this.state.year}</Text>
       <Text>The color is: {this.state.colors[0]}</Text>
       { leapyear }
+      <Display book={"Start React Native"}/>
+      <Display book={"Props test"}/>
+      <Display book={act_props}/>
+      <Display updateProp={()=>this.updateProps()} book={act_props2}/>
+      <Display2 updateProp={()=>this.updateProps()} book={act_props2}/>
     </View>
+    
     )
   }
+}
+
+class Display extends Component{
+  render(){
+    const {book,updateProp}=this.props
+    return(
+      <View>
+        <Text onPress={updateProp}>{book}</Text>
+      </View>
+    )
+  }
+}
+
+const Display2=(props)=>{
+  const {book,updateBook}=props
+  return(
+    <View>
+      <Text onPress={updateBook}>{book}</Text>
+    </View>
+  )
 }
 
 
